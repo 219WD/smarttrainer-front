a6b09535dc1c0bcad8739e83bf1843a4b0bea822
219WD <macgunmanmc@gmail.com>
2024-12-31 15:12:17 -0300

    instale react router dom, helmet para el SEO e hice el preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, lazy } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import Preload from './pages/Preload.jsx'
+
+const HomeScreen = lazy(() => import('./pages/HomeScreen'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Helmet>
+        <title>Smart Trainer | Entrena de Forma Inteligente</title>
+        <link rel="icon" type="image/svg+xml" href="/smart-trainer-logo.svg" />
+        <meta name="description" content="Smart Trainer es la aplicación ideal para planificar y seguir tus rutinas de entrenamiento de forma inteligente. Optimiza tus entrenamientos y alcanza tus metas más rápido." />
+        <meta name="keywords" content="Smart Trainer, Rutinas de Entrenamiento, Fitness, Salud, Ejercicio Inteligente" />
+        <meta name="author" content="Smart Trainer Team" />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "MobileApplication",
+            "name": "Smart Trainer",
+            "operatingSystem": "Android/iOS",
+            "applicationCategory": "Health & Fitness",
+            "description": "Smart Trainer es una aplicación móvil que te ayuda a gestionar tus entrenamientos de manera eficiente y personalizada.",
+            "url": "https://smarttrainer.app/",
+          })}
+        </script>
+      </Helmet>
+      <Suspense
+        fallback={<Preload />}
+      >
+        <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
